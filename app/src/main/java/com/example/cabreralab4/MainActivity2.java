a38a0690e2fb53385dc737083c4f679eaf763c12
FranceCabrera <fcabrera.a12241802@umak.edu.ph>
2024-04-26 20:21:28 +0800

    Final Commit

---
+++ app/src/main/java/com/example/cabreralab4/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.cabreralab4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textViewSecond;
+    Button backPageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewSecond = findViewById(R.id.textViewSecond);
+        backPageButton = findViewById(R.id.backPageButton);
+
+        backPageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FirstPage.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/cabreralab4/ThirdGuidedExercise.java
@@ -0,0 +1,39 @@
+package com.example.cabreralab4;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThirdGuidedExercise extends AppCompatActivity {
+
+    EditText editNum1, editNum2;
+    Button sumButton, averageButton;
+    double
+    Toast
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third_guided_exercise);
+    private boolean validateInput() {
+        if(editNum1.getText().toString().isEmpty() || editNum2.getText().toString().isEmpty())
+            {
+
+            }
+        }
+        else if
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
